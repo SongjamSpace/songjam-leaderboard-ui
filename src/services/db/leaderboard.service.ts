287a19e671d1dd67ef990892c53fddf3c856befc
logesh2496 <logesh.r24@gmail.com>
2025-07-22 19:40:24 +0530

    feat: eva lb check, submit for airdrop

--- src/services/db/leaderboard.service.ts
+++ src/services/db/leaderboard.service.ts
@@ -1,11 +1,12 @@
 import { db } from '../firebase.service';
-import { collection, doc, getDoc, setDoc } from 'firebase/firestore';
+import { collection, doc, getDoc } from 'firebase/firestore';
 
 const SONGJAM_LEADERBOARD_COLLECTION_NAME = 'singPointsLeaderboard_19_06_2025';
+const EVA_LEADERBOARD_COLLECTION_NAME = 'evaonlinexyz_leaderboard';
 
-const userIdExistsInLeaderboard = async (userId: string) => {
+const userIdExistsInLeaderboard = async (userId: string, id: 'SANG' | 'EVA') => {
   const docRef = doc(
-    collection(db, SONGJAM_LEADERBOARD_COLLECTION_NAME),
+    collection(db, id === 'SANG' ? SONGJAM_LEADERBOARD_COLLECTION_NAME : EVA_LEADERBOARD_COLLECTION_NAME),
     userId
   );
   const docSnap = await getDoc(docRef);
--- src/services/db/sangClaim.service.ts
+++ src/services/db/sangClaim.service.ts
@@ -1,31 +1,35 @@
-import { collection, doc, getDoc, serverTimestamp } from 'firebase/firestore';
+import { doc, getDoc, serverTimestamp } from 'firebase/firestore';
 
 import { setDoc } from 'firebase/firestore';
 import { db } from '../firebase.service';
 
 const CLAIM_SANG_TOKENS_COLLECTION = 'sang_airdrop_wallets';
+const CLAIM_EVA_TOKENS_COLLECTION = 'eva_airdrop_wallets';
 
-export interface SangSubmitWallet {
+export interface TokenSubmitWallet {
   walletAddress: string;
   userId: string;
   username: string | null;
   name: string | null;
 }
 
-export const submitWalletForAirdrop = async (
+export const submitTokenForAirdrop = async (
   twitterId: string,
-  claimSangTokens: SangSubmitWallet
+  claimTokens: TokenSubmitWallet,
+  id: 'SANG' | 'EVA'
 ) => {
-  const claimSangTokensRef = doc(db, CLAIM_SANG_TOKENS_COLLECTION, twitterId);
-  await setDoc(claimSangTokensRef, {
-    ...claimSangTokens,
+  const collectionName = id === 'SANG' ? CLAIM_SANG_TOKENS_COLLECTION : CLAIM_EVA_TOKENS_COLLECTION;
+  const claimTokensRef = doc(db, collectionName, twitterId);
+  await setDoc(claimTokensRef, {
+    ...claimTokens,
     createdAt: Date.now(),
     createdDateTime: serverTimestamp(),
   });
 };
 
-export const getWalletForAirdrop = async (twitterId: string) => {
-  const claimSangTokensRef = doc(db, CLAIM_SANG_TOKENS_COLLECTION, twitterId);
-  const claimSangTokens = await getDoc(claimSangTokensRef);
-  return claimSangTokens.data();
+export const getWalletForAirdrop = async (twitterId: string, id: 'SANG' | 'EVA') => {
+  const collectionName = id === 'SANG' ? CLAIM_SANG_TOKENS_COLLECTION : CLAIM_EVA_TOKENS_COLLECTION;
+  const claimTokensRef = doc(db, collectionName, twitterId);
+  const claimTokens = await getDoc(claimTokensRef);
+  return claimTokens.data();
 };
